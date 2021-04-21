53f668be2d59a023610368e6ca48ff32bf0029d7
baltaz <baltazar.crstn@gmail.com>
2021-04-21 14:36:12 -0300

    main physic resolved

--- script.js
+++ script.js
@@ -1,128 +1,104 @@
+import {stages} from './stages.js';
+import {getBlockColor} from './utils.js';
+
 const stage = document.querySelector(".stage");
-const blocks = [];
-const blocksData = [];
-const map = 0;
-const selectorData = {
-    row:1,
-    column:1,
-    selected: false
-}
+const level = 0;
 
-let selectedBlock = null;
+const selector = { row:0, column:0, selected: false}
 
-const stages =
-[
-    [
-        {row:1,column:2,color:"gray"},
-        {row:2,column:4,color:"gray"},
-        {row:3,column:5,color:"red"},
-        {row:4,column:6,color:"blue"},
-        {row:1,column:3,color:"red"}
-    ],
-    [
-        {row:2,column:8,color:"gray"},
-        {row:2,column:2,color:"gray"},
-        {row:4,column:5,color:"red"},
-        {row:2,column:1,color:"blue"},
-        {row:5,column:5,color:"red"}
-    ],
-]
-
-const createBlock = (blockData) =>{
-    const block = document.createElement("div");
-    block.classList.add("block");
-    block.style.gridArea=`${blockData.row}/${blockData.column}`;
-    block.style.backgroundColor=blockData.color;
-    stage.appendChild(block);
-    blocks.push(block);
-    blocksData.push(block);
-}
-
-
-const createMap = level =>{
-    blocks.forEach(block=>{
-        block.remove();
-        blocks.shift;
-    });
-    stages[level].forEach(e=>createBlock(e))
+const createMap = () =>{
+    for (let row = 0; row<12; row++) {
+        for(let column=0; column<10; column++){
+            const block = document.createElement("div");
+            block.classList.add("block");
+            block.id=`${row}${column}`;
+            block.style.gridArea=`${row+1}/${column+1}`;
+            block.style.backgroundColor = getBlockColor(grid[row][column]);
+            stage.appendChild(block);
+        }
+    }
 }
 
 const createSelector = () =>{
-    const selector = document.createElement("div");
-    selector.classList.add("selector");
-    stage.appendChild(selector);
-    return selector;
+    const selectorNode = document.createElement("div");
+    selectorNode.classList.add("selector");
+    selectorNode.style.gridArea=`${selector.row+1}/${selector.column+1}`;
+    stage.appendChild(selectorNode);
+    return selectorNode;
 }
-
-
-createMap(map);
-const selector = createSelector()
 
 const select = () =>{
-    if(selectorData.selected){
-        selectorData.selected = false;
-        selector.classList.remove("selected");
-        selectedBlock=null;
+    if(selector.selected){
+        selector.selected = false;
+        selectorNode.classList.remove("selected");
     }else{
-        blocks.forEach(block=>{                                 //check with for
-            let blockCol = block.style.gridColumn[0];
-            let blockRow = block.style.gridRow[0];
-            if(blockCol == selectorData.column && blockRow==selectorData.row){
-                selectedBlock=block;
-                selectorData.selected = true;
-                selector.classList.add("selected");
-            }
-        })
+        const block = grid[selector.row][selector.column];
+        if(block && block!=9){
+            selector.selected = true;
+            selectorNode.classList.add("selected");
+        }
     }
 }
 
-const move = direction =>{ //CODE REVIEW!!!!
+const editBlock = (row,column,colorId) =>{
+    const block = document.getElementById (`${row}${column}`)
+    block.style.backgroundColor = getBlockColor(colorId);
+}
+const swapBlocks = (row,column,blockColor,direction) =>{ //unificar swap / gravity y checkCollision?
+    if(direction=="down"){
+        editBlock(row+1,column,blockColor);
+        grid[row+1][column]=grid[row][column];
+    }else{
+        let i = (direction=="right")? 1 : -1;
+        editBlock(row,column+i,blockColor);
+        grid[row][column+i]=grid[row][column];
+    }
+    editBlock(row,column,0);
+    grid[row][column]=0;
+}
+
+const gravityFall = (row, column, blockColor)=>{ //agregar un dalay en la caida
+    for (row; row < 11; row++) {
+        if(!(grid[row+1][column])){
+            if(selector.row==row){selector.row++}
+            swapBlocks(row,column,blockColor,"down");
+            
+        }
+    }
+}
+
+const checkCollision = (row, column, direction)=>{
+    let i = (direction=="right")? 1 : -1;
+    if(!(grid[row][column+i])){
+        swapBlocks(row, column, grid[row][column],direction); //unificar SWAP con gravity
+        for (let prevrow=row; prevrow > 1; prevrow--) {
+            if(grid[prevrow-1][column] && grid[prevrow-1][column]!=9){
+                gravityFall(prevrow-1,column, grid[prevrow-1][column])
+            }
+        }
+        column +=i;
+        selector.column +=i;
+        gravityFall(row, column, grid[row][column]);
+    }
+}
+
+const move = direction =>{
     switch (direction) {
-        case "up":
-            if(selectorData.row>1){
-                selectorData.row -=1;
-                if(selectedBlock){
-                    selectedBlock.style.gridArea = `${Number(selectedBlock.style.gridRow[0])-1}/${selectedBlock.style.gridColumn}`
-                    console.log(selectedBlock.style.gridArea);
-                    console.log(selectorData.row+" y "+selectorData.column);
-                }
-            }
-            break;
-        case "right":
-            if(selectorData.column<10){
-                selectorData.column +=1
-                if(selectedBlock){
-                    selectedBlock.style.gridArea = `${Number(selectedBlock.style.gridRow[0])}/${Number(selectedBlock.style.gridColumn[0])+1}`
-                    console.log(selectedBlock.style.gridArea);
-                    console.log(selectorData.row+" y "+selectorData.column);
-                }
-            }
-            break;
-        case "down":
-            if(selectorData.row<10){
-                selectorData.row +=1
-                if(selectedBlock){
-                    selectedBlock.style.gridArea = `${Number(selectedBlock.style.gridRow[0])+1}/${selectedBlock.style.gridColumn}`
-                    console.log(selectedBlock.style.gridArea);
-                    console.log(selectorData.row+" y "+selectorData.column);
-                }
-            }
+        case "up": if(selector.row>0 && !selector.selected)selector.row -=1; break;
+        case "down": if(selector.row<11 && !selector.selected)selector.row +=1; break;
+        case "right": //UNIFICAR EN UNA MISMA FUNCION RIGHT Y LEFT SOLO CAMBIA +/-1
+            if(selector.column<9)
+                if(selector.selected) checkCollision(selector.row,selector.column,"right");
+                else selector.column +=1;
             break;
         case "left":
-            if(selectorData.column>1){
-                selectorData.column -=1
-                if(selectedBlock){
-                    selectedBlock.style.gridArea = `${Number(selectedBlock.style.gridRow[0])}/${Number(selectedBlock.style.gridColumn[0])-1}`
-                    console.log(selectedBlock.style.gridArea);
-                    console.log(selectorData.row+" y "+selectorData.column);
-                }
-            }
+            if(selector.column>0)
+                if(selector.selected) checkCollision(selector.row,selector.column,"left");
+                else selector.column -=1;
             break;
     }
+    selectorNode.style.gridArea=`${selector.row+1}/${selector.column+1}`;
 }
-
-
-
 
 document.addEventListener("keydown", e=>{
     switch (e.key) {
@@ -133,14 +109,17 @@ document.addEventListener("keydown", e=>{
         case " ": select(); break; //spacebar
         default: break;
     }
-    selector.style.gridArea=`${selectorData.row}/${selectorData.column}`;
 })
 
+//RUN
+let grid = stages[level];
+createMap();
+const selectorNode = createSelector();
 
-//move a block
-//collapse blocks (limits)
-//collapse blocks same color (POINT)
-//gravity
-//reset level
-//score
+//disapear collapsed blocks with same color (POINT)
+//let move selector only on block != 9
+//////////////////////////////////////
+//pending blocks panel
+//score panel
+//combos
 //open stages from JSON
---
+++ utils.js
@@ -0,0 +1,17 @@
+export const getBlockColor = id=> {
+    let color = ""
+    switch (id) {
+        case 0: color = ""; break;
+        case 1: color = "SkyBlue"; break;
+        case 2: color = "SpringGreen"; break;
+        case 3: color = "Red"; break;
+        case 4: color = "YellowGreen"; break;
+        case 5: color = "Teal"; break;
+        case 6: color = "Tomato"; break;
+        case 7: color = "Coral"; break;
+        case 8: color = "DarkTurquoise"; break;
+        case 9: color = "black"; break;
+        default: break;
+    }
+    return color;
+}
