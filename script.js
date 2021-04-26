689ae80c6751d3d126cf474310dc74aa16b9b860
baltaz <baltazar.crstn@gmail.com>
2021-04-25 22:07:07 -0300

    first functional version with refactor finished

--- script.js
+++ script.js
@@ -1,139 +1,156 @@
-import {stages} from './stages.js';
-import {getBlockColor} from './utils.js';
+import { stages } from "./stages.js";
+import { getBlockColor } from "./utils.js";
 
 const stage = document.querySelector(".stage");
+const score = document.querySelector(".score");
 let level = 0;
 let points = 0;
-
-const selector = { row:0, column:0, selected: false}
-
-const createMap = () =>{
-    for (let row = 0; row<12; row++) {
-        for(let column=0; column<10; column++){
-            if(grid[row][column]){ //si no esta vacio
-                const block = document.createElement("div");
-                block.classList.add("block");
-                block.id=`${row}${column}`;
-                block.style.gridArea=`${row+1}/${column+1}`;
-                block.style.backgroundColor = getBlockColor(grid[row][column]);
-                stage.appendChild(block);
-            }
-        }
-    }
-}
-
-const createSelector = () =>{
-    const selectorNode = document.createElement("div");
-    selectorNode.classList.add("selector");
-    selectorNode.style.gridArea=`${selector.row+1}/${selector.column+1}`;
-    stage.appendChild(selectorNode);
-    return selectorNode;
-}
-
-const select = () =>{
-    if(selector.selected){
-        selector.selected = false;
-        selectorNode.classList.remove("selected");
-    }else{
-        const block = grid[selector.row][selector.column];
-        if(block && block!=9){
-            selector.selected = true;
-            selectorNode.classList.add("selected");
-        }
-    }
-}
-
-const editBlock = (row,column,colorId) =>{
-    const block = document.getElementById (`${row}${column}`)
-    block.style.backgroundColor = getBlockColor(colorId);
-}
-
-const swapBlocks = (row,column,blockColor,direction) =>{ //unificar swap / gravity y checkCollision?
-    if(direction=="down"){
-        editBlock(row+1,column,blockColor);
-        grid[row+1][column]=grid[row][column];
-    }else{
-        let i = (direction=="right")? 1 : -1;
-        editBlock(row,column+i,blockColor);
-        grid[row][column+i]=grid[row][column];
-    }
-    editBlock(row,column,0);
-    grid[row][column]=0;
-}
-
-const gravityFall = (row, column, blockColor)=>{ //agregar un dalay en la caida
-    if(!(grid[row+1][column])){
-        if(selector.row==row) selector.row++; //si no es el bloque movido    
-        swapBlocks(row,column,blockColor,"down"); 
-        setTimeout(e=>{gravityFall(++row,column,blockColor)},200);
-    }else{
-        checkMatch(row, column, blockColor);
-    }
-}
-
-const checkCollision = (row, column, direction)=>{
-    let i = (direction=="right")? 1 : -1;
-    if(!(grid[row][column+i])){
-        swapBlocks(row, column, grid[row][column],direction); //unificar SWAP con gravity
-        for (let prevrow=row; prevrow > 1; prevrow--) {
-            if(grid[prevrow-1][column] && grid[prevrow-1][column]!=9){
-                setTimeout(e=>{gravityFall(prevrow-1,column,grid[prevrow-1][column])},200);
-            }
-        }
-        column +=i;
-        selector.column +=i;
-        setTimeout(e=>{gravityFall(row,column,grid[row][column])},200);
-        //checkear para todos los bloques si hay matching
-    }
-}
-
-
-const deleteBlock = (row,column)=>{
-    editBlock(row,column,0);
-    grid[row][column]=0;
-}
-
-
-const checkMatch = (row, column, colorBlock) =>{
-    if((grid[row+1][column]==colorBlock) || (grid[row][column+1]==colorBlock) || (grid[row][column-1]==colorBlock)){
-        if(grid[row+1][column]==colorBlock) deleteBlock(row+1,column);                    
-        if(grid[row][column+1]==colorBlock) deleteBlock(row,column+1);
-        if(grid[row][column-1]==colorBlock) deleteBlock(row,column-1);
-
-        deleteBlock(row,column);
-        selector.selected=false;                            //crear funcion "descelect"
-        selectorNode.classList.remove("selected");          //crear funcion "descelect"
-        const score = document.querySelector(".score");
-        score.textContent = ++points;
-    }
-}
-
-const move = direction =>{
-    if(selector.selected) checkCollision(selector.row,selector.column,direction);
-    else selector.column +=(direction=="right")?1:-1;
-}
-
-document.addEventListener("keydown", e=>{
-    switch (e.key) {
-        case "ArrowUp": if(selector.row>0 && !selector.selected)selector.row -=1; break;
-        case "ArrowDown": if(selector.row<11 && !selector.selected)selector.row +=1;break;
-        case "ArrowRight": if(selector.column<9) move("right");break
-        case "ArrowLeft": if(selector.column>0) move("left");break
-        case " ": select(); break; //spacebar
-        default: break;
-    }
-    selectorNode.style.gridArea=`${selector.row+1}/${selector.column+1}`;
-})
-
-//RUN
+let falling = false;
 let grid = stages[level];
+const selector = { row: 6, col: 7, selected: false };
+
+const createBlock = (row, col, color) => {
+  const block = document.createElement("div");
+  block.classList.add("block");
+  block.id = `${row}${col}`;
+  block.style.gridArea = `${row + 1}/${col + 1}`;
+  block.style.backgroundColor = getBlockColor(color);
+  stage.appendChild(block);
+};
+
+const createMap = () => {
+  for (let row = 0; row < 12; row++)
+    for (let col = 0; col < 10; col++)
+      if (grid[row][col]) createBlock(row, col, grid[row][col]);
+};
+
+const createSelector = () => {
+  const selectorNode = document.createElement("div");
+  selectorNode.classList.add("selector");
+  selectorNode.style.gridArea = `${selector.row + 1}/${selector.col + 1}`;
+  stage.appendChild(selectorNode);
+  return selectorNode;
+};
+
+const toggleSelector = () => {
+  selector.selected = !selector.selected;
+  selectorNode.classList.toggle("selected");
+};
+
+const select = () => {
+  if (selector.selected) toggleSelector();
+  else if (grid[selector.row][selector.col] > 1) toggleSelector();
+};
+
+const deleteBlock = (row, col) => {
+  document.getElementById(`${row}${col}`).remove();
+  grid[row][col] = 0;
+};
+
+const checkMatch = (row, col) => {
+  if (
+    grid[row + 1][col] == grid[row][col] ||
+    grid[row][col + 1] == grid[row][col] ||
+    grid[row][col - 1] == grid[row][col]
+  ) {
+    if (grid[row + 1][col] == grid[row][col]) deleteBlock(row + 1, col);
+    if (grid[row][col + 1] == grid[row][col]) deleteBlock(row, col + 1);
+    if (grid[row][col - 1] == grid[row][col]) deleteBlock(row, col - 1);
+    deleteBlock(row, col);
+    toggleSelector();
+    score.textContent = ++points;
+  }
+};
+
+const updateGridArea = (newRow, newCol, oldRow, oldCol) => {
+  const block = document.getElementById(`${oldRow}${oldCol}`);
+  grid[newRow][newCol] = grid[oldRow][oldCol];
+  grid[oldRow][oldCol] = 0;
+  block.style.gridArea = `${newRow + 1}/${newCol + 1}`;
+  block.id = `${newRow}${newCol}`;
+};
+
+const moveBlock = (row, col, direction) => {
+  switch (direction) {
+    case "down":
+      updateGridArea(row + 1, col, row, col);
+      break;
+    case "right":
+      updateGridArea(row, col + 1, row, col);
+      break;
+    case "left":
+      updateGridArea(row, col - 1, row, col);
+      break;
+    default:
+      break;
+  }
+};
+
+const gravityFall = (row, col) => {
+  if (!grid[row + 1][col]) {
+    if (selector.row == row) {
+      ++selector.row;
+      selectorNode.style.gridArea = `${selector.row + 1}/${selector.col + 1}`;
+    }
+    moveBlock(row, col, "down");
+    setTimeout((e) => {
+      gravityFall(++row, col);
+    }, 100);
+  } else {
+    falling = false;
+    checkMatch(row, col);
+  }
+};
+
+const checkCollision = (row, col, direction) => {
+  let i = direction == "right" ? 1 : -1;
+  if (!grid[row][col + i]) {
+    moveBlock(row, col, direction);
+    falling = true;
+    col += i;
+    selector.col += i;
+    setTimeout((e) => {
+      gravityFall(row, col);
+    }, 100);
+    for (let prevrow = row - 1; prevrow >= 0; prevrow--)
+      if (grid[prevrow][col - i] > 1)
+        setTimeout((e) => {
+          gravityFall(prevrow, col - i);
+        }, 100);
+  }
+};
+
+const move = (direction) => {
+  if (selector.selected) checkCollision(selector.row, selector.col, direction);
+  else selector.col += direction == "right" ? 1 : -1;
+};
+
+document.addEventListener("keydown", (e) => {
+  if (!falling) {
+    switch (e.key) {
+      case "ArrowUp":
+        if (selector.row > 0 && !selector.selected && !falling)
+          selector.row -= 1;
+        break;
+      case "ArrowDown":
+        if (selector.row < 11 && !selector.selected && !falling)
+          selector.row += 1;
+        break;
+      case "ArrowRight":
+        if (selector.col < 9 && !falling) move("right");
+        break;
+      case "ArrowLeft":
+        if (selector.col > 0 && !falling) move("left");
+        break;
+      case " ":
+        select();
+        break;
+      default:
+        break;
+    }
+    selectorNode.style.gridArea = `${selector.row + 1}/${selector.col + 1}`;
+  }
+});
+
 createMap();
 const selectorNode = createSelector();
-
-//TO DO 
-//disapear collapsed blocks with same color (POINT)
-//////////////////////////////////////
-//let move selector only on block != 9
-//pending blocks panel
-//score panel
-//combos
--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ export const getBlockColor = id=> {
     let color = ""
     switch (id) {
         case 0: color = ""; break;
-        case 1: color = "SkyBlue"; break;
+        case 1: color = "black"; break;
         case 2: color = "SpringGreen"; break;
         case 3: color = "Red"; break;
         case 4: color = "YellowGreen"; break;
@@ -10,7 +10,7 @@ export const getBlockColor = id=> {
         case 6: color = "Tomato"; break;
         case 7: color = "Coral"; break;
         case 8: color = "DarkTurquoise"; break;
-        case 9: color = "black"; break;
+        case 9: color = "SkyBlue"; break;
         default: break;
     }
     return color;
